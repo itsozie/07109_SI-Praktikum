5e808f9c6dada9c054e8773d0becfb7abdc17d1c
Ozie <itsozie77.com>
2021-05-17 23:43:14 +0700

    8.1.5 ubah file DaftarprakModel.php

--- Model/DaftarPrakModel.php
+++ Model/DaftarPrakModel.php
@@ -30,9 +30,66 @@
         extract($data);
         require_once("VIew/daftarprak/index.php");
     }
+    /**
+     * @param integer $id berisi id
+     * @param integer $id_aslab berisi id aslab
+     * function ini digunakan untuk mengupdate status daftarprak
+     */
+    public function prosesVerif($id, $idAslab){
+        $sql = "
+                UPDATE daftarprak SET status = 1,
+                aslab_id = $idAslab WHERE id = $id
+        ";
+        $query = koneksi()->query($sql);
+        return $query;
+    }
 
+    /**
+     * @param integer id berisi $id
+     * @param integer idPraktikan berisi id praktikan
+     * function unverif digunakan untuk meng un verif status dafarprak
+     */
+    public function prosesUnVerif($id, $idPraktikan){
+        $sqlDelete = "DELETE FROM nilai WHERE praktikan_id = $idPraktikan";
+        koneksi()->query($sqlDelete);
+
+        $sqlUpdate = "UPDATE daftarprak SET status = 0, aslab_id = NULL
+        WHERE id = $id";
+
+        $query = koneksi()->query($sqlUpdate);
+        return $query;
+    }
+
+    /**
+     * function verif berfungsi untuk mengverifikasi praktikan 
+     * yang sudah mendaftar praktikum
+     */
+    public function verif(){
+        $id = $_GET['id'];
+        $idAslab = $_GET['aslab']['id'];
+        if ($this->prosesVerif($id,$idAslab)) {
+            header("location: index.php?page=daftarprak&aksi=view&pesan=Berhasil Verifikasi Praktikan");
+        }else {
+            header("location: index.php?page=daftarprak&aksi=view&pesan=Gagal Verifikasi Praktikan");
+        }
+    }
+
+     /**
+     * function Unverif berfungsi untuk mengUnverifikasi praktikan 
+     * yang sudah mendaftar praktikum
+     */
+    public function Unverif(){
+        $id = $_GET['id'];
+        $idAslab = $_GET['idPraktikan'];
+        if ($this->prosesUnVerif($id,$idPraktikan)) {
+            header("location: index.php?page=daftarprak&aksi=view&pesan=Berhasil UnVerifikasi Praktikan");
+        }else {
+            header("location: index.php?page=daftarprak&aksi=view&pesan=Gagal UnVerifikasi Praktikan");
+        }
+    }
 }
 
+
+
 // $tes = new DaftarPrakModel();
-// var_export($tes->get());
-// die();
+// var_export($tes->prosesVerif(2,2));die();
